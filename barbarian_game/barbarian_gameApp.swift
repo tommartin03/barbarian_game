39cf98647ecd4dfba5ac95c78c5c49b60e5d3d69
tplocal <tplocal@MacBook-Air-de-tplocal.local>
2025-12-02 13:33:59 +0100

    Initialisation du projet

---
+++ barbarian_game/barbarian_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  barbarian_gameApp.swift
+//  barbarian_game
+//
+//  Created by tplocal on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct barbarian_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
